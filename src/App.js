c290a1e3ef302060ff1c407b360ff9cbca63d2b1
NotEmily3203 <linemily62@gmail.com>
2023-11-11 16:15:45 -0500

    openai works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Auth } from "./pages/auth/index";
-import { Questionaire } from "./pages/workout-tracker/index";
+import Questionaire from "./pages/workout-tracker/index";
 
 function App() {
   return (
--- src/pages/workout-tracker/index.jsx
+++ src/pages/workout-tracker/index.jsx
@@ -3,6 +3,113 @@ import { auth } from "../../config/firebase-config";
 import { useNavigate } from "react-router-dom";
 import { useGetUserInfo } from "../../hooks/useGetUserInfo";
 import { signOut } from "firebase/auth";
+import axios from "axios";
+import './style.css';
+
+const Questionaire = () => {
+    const navigate = useNavigate();
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    const { name, profilePhoto } = useGetUserInfo();
+
+    const signUserOut = async () => {
+        try {
+            await signOut(auth);
+            localStorage.clear();
+            navigate("/");
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
+    
+    const createPrompt = (event) => {
+        let weight = document.getElementById('Weight').value;
+        let height = document.getElementById('Height').value;
+        let currentActivityLevel = document.getElementById('Current-Activity-Level').value;
+        let fitnessGoal = document.getElementById('Fitness-Goal').value;
+        let medical = document.getElementById('Medical').value;
+        let prompt = "Create a weekly workout plan based on the following information: \nWeight: " + weight + " pounds\nHeight: " + height + " inches\nCurrent Activity Level: " + currentActivityLevel + "\nFitness Goal: " + fitnessGoal + "\nPre-existing Medical Conditions: " + medical;
+        event.preventDefault();
+        console.log(prompt);
+
+        // Make the API call
+        callOpenAI(prompt);
+    };
+
+    const callOpenAI = async (prompt) => {
+        try {
+            const openaiEndpoint = 'https://api.openai.com/v1/engines/davinci/completions';
+            const openaiApiKey = process.env.REACT_APP_OPENAI_API_KEY; // Make sure to replace this with your actual API key
+
+            const response = await axios.post(
+                openaiEndpoint,
+                {
+                    prompt: prompt,
+                    max_tokens: 150 // Adjust as needed
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${openaiApiKey}`
+                    }
+                }
+            );
+
+            const workout = response.data.choices[0].text.trim();
+            console.log(`Workout:`);
+            console.log(workout);
+        } catch (error) {
+            console.error('Error calling OpenAI:', error.response ? error.response.data : error.message);
+        }
+    };
+
+    return (
+        <>
+            <div className="questionaire-page">
+                <div className="profile">
+                    <img className="profile-photo" src={profilePhoto} alt="Profile" />
+                    <p>{name}</p>
+                    <button className="sign-out-button" onClick={signUserOut}>
+                        Sign Out
+                    </button>
+                </div>
+
+                <form className="questionaire-container" onSubmit={createPrompt}>
+                    <label htmlFor="Gender">Gender</label>
+                <select id="Gender" name="gender" value={selectedOption} onChange={handleChange}>
+                    <option value="">Not Listed</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                </select>
+                    <label htmlFor="Weight">Weight (lbs)</label>
+                    <input type = "text" id = "Weight" required></input>
+                    <label htmlFor="Height">Height (in)</label>
+                    <input type = "text" id = "Height" required></input>
+                    <label htmlFor="Current-Activity-Level">Current Activity Level</label>
+                    <input type = "text" id = "Current-Activity-Level" required></input>
+                    <label htmlFor="Fitness-Goal">Fitness Goal</label>
+                    <input type = "text" id = "Fitness-Goal" required></input>
+                    <label htmlFor="Medical">Medical Conditions/Injuries</label>
+                    <input type = "text" id = "Medical" required></input>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        </>
+    );
+};
+export default Questionaire;
+/*
+import { useState } from "react";
+import { auth } from "../../config/firebase-config";
+import { useNavigate } from "react-router-dom";
+import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import { signOut } from "firebase/auth";
 import './style.css';
 
 export const Questionaire = () =>{
@@ -45,12 +152,12 @@ export const Questionaire = () =>{
             <label htmlFor="Weight">Weight (lbs)</label>
             <input type = "text" id = "Weight" required></input>
             <label htmlFor="Height">Height (in)</label>
-            <input type = "text" id = "Weight" required></input>
+            <input type = "text" id = "Height" required></input>
             <label htmlFor="Current-Activity-Level">Current Activity Level</label>
             <input type = "text" id = "Current-Activity-Level" required></input>
             <label htmlFor="Fitness-Goal">Fitness Goal</label>
             <input type = "text" id = "Fitness-Goal" required></input>
-            <label htmlFor="Fitness-Goal">Medical Conditions/Injuries</label>
+            <label htmlFor="Medical">Medical Conditions/Injuries</label>
             <input type = "text" id = "Medical" required></input>
             <input type="Submit"></input>
         </form>
@@ -58,3 +165,4 @@ export const Questionaire = () =>{
         </>
     )
 }
+*/
